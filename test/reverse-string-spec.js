d39b7e22910f673214ca4017c9fc432b541b41fa
Logan French <Loganfrench04@gmail.com>
2023-09-13 19:39:20 -0400

    Reverse string green and nopm chai mocha

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,10 @@
-// Your code here
+const { expect } = require("chai");
+const reverseString = require('../problems/reverse-string')
+
+describe('reverseString', function () {
+    it('Should take a string and reverse it', function () {
+        let string = 'fun'
+        let reverse = reverseString(string)
+        expect(reverse).to.equal('nuf')
+    })
+})
