355c4264614a7bdff6139a0db42a468fb6ab785d
Samuel <187swachira@gmail.com>
2023-09-14 17:03:01 -0400

    number-fun complete

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,10 +1,17 @@
 function returnsThree() {
   // Your code here
+  return 3 
 }
 
 function reciprocal(n) {
   // Your code here
+  if (typeof n !== 'number') {
+    throw new TypeError('arguments can only be numbers')
+  }
+  return 1 / n
 }
+
+console.log(reciprocal(23))
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,28 @@
-// Your code here
+const {expect, assert} = require('chai')
+const {returnsThree} = require('../problems/number-fun')
+const {reciprocal} = require('../problems/number-fun')
+
+describe('returnsThree', function (){
+    it('should return 3', function (){
+        let num = returnsThree()
+        expect(num).to.equal(3)
+    })
+
+})
+
+describe('reciprocal(num)', function (){
+    it('should intake a number and then return the reciprocal of that number', function (){
+        let num = reciprocal(4)
+        expect(num).to.equal(0.25)
+
+    })
+
+    it('it should throw and error with invalid arguments', function (){
+      
+        let invalidArg = 'invalidArg'
+        let testFunction = () => reciprocal(invalidArg)
+
+        expect(testFunction).to.throw(TypeError, 'arguments can only be numbers')
+
+    })
+})
