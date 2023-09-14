4c04da699fac601ed25c6e7f61f609c9e0e0677e
Logan French <Loganfrench04@gmail.com>
2023-09-14 18:41:55 -0400

    phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,17 +1,21 @@
 function returnsThree() {
   // Your code here
-  return 3 
+  return 3
 }
 
 function reciprocal(n) {
   // Your code here
   if (typeof n !== 'number') {
     throw new TypeError('arguments can only be numbers')
+  } else if (n < 1) {
+    throw new RangeError('arguments can not be less than 1')
+  } else if (n > 1000000) {
+    throw new RangeError('arguments can not be greater than 1 million')
   }
   return 1 / n
 }
 
-console.log(reciprocal(23))
+console.log(reciprocal(1000))
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,28 +1,40 @@
-const {expect, assert} = require('chai')
-const {returnsThree} = require('../problems/number-fun')
-const {reciprocal} = require('../problems/number-fun')
+const { expect, assert } = require('chai')
+const { returnsThree } = require('../problems/number-fun')
+const { reciprocal } = require('../problems/number-fun')
 
-describe('returnsThree', function (){
-    it('should return 3', function (){
+describe('returnsThree', function () {
+    it('should return 3', function () {
         let num = returnsThree()
         expect(num).to.equal(3)
     })
 
 })
 
-describe('reciprocal(num)', function (){
-    it('should intake a number and then return the reciprocal of that number', function (){
+describe('reciprocal(num)', function () {
+    it('should intake a number and then return the reciprocal of that number', function () {
         let num = reciprocal(4)
         expect(num).to.equal(0.25)
 
     })
 
-    it('it should throw and error with invalid arguments', function (){
-      
+    it('it should throw and error with invalid arguments', function () {
+
         let invalidArg = 'invalidArg'
         let testFunction = () => reciprocal(invalidArg)
 
         expect(testFunction).to.throw(TypeError, 'arguments can only be numbers')
 
+    })
+
+    it('it should throw an error if num is less than 1', function () {
+        let invalidNum = 0
+        let testFunction = () => reciprocal(invalidNum)
+        expect(testFunction).to.throw(RangeError, 'arguments can not be less than 1')
+    })
+
+    it('it should throw an error if num is greater than 1 million', function () {
+        let invalidNum = 1000001
+        let testFunction = () => reciprocal(invalidNum)
+        expect(testFunction).to.throw(RangeError, 'arguments can not be greater than 1 million')
     })
 })
