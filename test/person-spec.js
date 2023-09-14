4e3a2dc7eb709745239b7302da91b3bf83588c2c
Samuel <187swachira@gmail.com>
2023-09-14 19:01:07 -0400

    constructor test completed

--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const Person = require('../problems/person')
+const {expect} = require('chai')
+
+describe('Person constructor', function (){
+    it ('should have a name and age properites and set them properly', function (){
+        let person = new Person('Logan', 50)
+
+        expect(person).to.have.property('name')
+        expect(person).to.have.property('age')
+
+        expect(person.name).to.equal('Logan')
+        expect(person.age).to.equal(50)
+
+
+    })
+})
